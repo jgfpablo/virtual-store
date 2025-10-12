11dac0e42486201eb21518cdfd5c89ed1c968d97
Pablo Frank <jgfpablo@gmail.com>
2025-10-12 11:49:41 -0300

    cambios

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -11,32 +11,122 @@ import { Product } from '../../interface/products';
 export class ListProductsComponent {
   @Input() category!: string;
   products: Product[] = [];
+
+  page: number = 1;
+  totalPages: number = 1;
+  visiblePages: number = 5; // cantidad de páginas a mostrar en el carrusel
+
   loading = true;
 
   constructor(private service: ProductsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadProducts();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-
     if (changes['category']) {
-      this.loading = true;
-      this.products = [];
-      if (this.category) {
-        console.log('entre a categorias');
-        this.service.getByCategory(this.category).subscribe((data) => {
-          this.products = Array.isArray(data) ? data : [data];
-          this.loading = false;
-        });
-      } else {
-        console.log('entre a all products');
-        this.service.getAll().subscribe((data) => {
-          this.products = data;
-          this.loading = false;
-        });
-      }
+      this.page = 1;
+      this.loadProducts();
     }
+
+    // if (changes['category']) {
+    //   this.loading = true;
+    //   this.products = [];
+    //   if (this.category) {
+    //     console.log('entre a categorias');
+    //     this.service.getByCategory(this.category, 1, 6).subscribe({
+    //       next: (res: any) => {
+    //         this.products = res.products || [];
+    //         this.loading = false;
+    //       },
+    //       error: (err) => {
+    //         console.error(err);
+    //         this.loading = false;
+    //       },
+    //     });
+    //   } else {
+    //     console.log('entre a all products');
+    //     this.service.getAll().subscribe({
+    //       next: (res: any) => {
+    //         this.products = res.products || [];
+    //         this.loading = false;
+    //       },
+    //       error: (err) => {
+    //         console.error(err);
+    //         this.loading = false;
+    //       },
+    //     });
+    //   }
+    // }
+  }
+
+  loadProducts() {
+    this.loading = true;
+    this.products = [];
+
+    console.log(this.page);
+    if (this.category) {
+      this.service.getByCategory(this.category, this.page, 6).subscribe({
+        next: (res: any) => {
+          this.products = res.products;
+          this.totalPages = res.totalPages;
+          this.loading = false;
+        },
+        error: (err: any) => {
+          console.error(err);
+          this.loading = false;
+        },
+      });
+    } else {
+      this.service.getAll(this.page, 6).subscribe({
+        next: (res: any) => {
+          this.products = res.products;
+          this.totalPages = res.totalPages;
+          this.loading = false;
+        },
+        error: (err: any) => {
+          console.error(err);
+          this.loading = false;
+        },
+      });
+    }
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.page = page;
+      this.loadProducts();
+    }
+  }
+
+  nextPage() {
+    if (this.page < this.totalPages) {
+      this.page++;
+      this.loadProducts();
+    }
+  }
+
+  prevPage() {
+    if (this.page > 1) {
+      this.page--;
+      this.loadProducts();
+    }
+  }
+
+  getPages(): number[] {
+    const pages: number[] = [];
+    const half = Math.floor(this.visiblePages / 2);
+    let start = Math.max(this.page - half, 1);
+    let end = Math.min(start + this.visiblePages - 1, this.totalPages);
+
+    // ajustar start si estamos al final
+    start = Math.max(end - this.visiblePages + 1, 1);
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    return pages;
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -7,22 +7,25 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ProductsService {
-  private apiUrl = 'https://virtual-store-api-xm81.onrender.com/api/products'; // tu endpoint
+  private apiUrl =
+    'https://virtual-store-api-production.up.railway.app/api/products/'; // tu endpoint
 
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+  getAll(page: number = 1, limit: number = 6) {
+    return this.http.get<{
+      products: Product[];
+      total: number;
+      totalPages: number;
+    }>(`${this.apiUrl}?page=${page}&limit=${limit}`);
   }
 
-  getByCategory(
-    category: string,
-    page: number,
-    limit: number
-  ): Observable<Product[]> {
-    return this.http.get<Product[]>(
-      `${this.apiUrl}/categoria/${category}?page=${page}&limit=${limit}`
-    );
+  getByCategory(category: string, page: number = 1, limit: number = 6) {
+    return this.http.get<{
+      products: Product[];
+      total: number;
+      totalPages: number;
+    }>(`${this.apiUrl}categoria/${category}?page=${page}&limit=${limit}`);
   }
 
   getById(id: string): Observable<Product> {
